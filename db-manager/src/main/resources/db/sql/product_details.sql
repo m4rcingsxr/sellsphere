180ab78ca4808cc195febe1568b2c32c2a7c4205
Marcin Seweryn <marcinsewerynn@gmail.com>
2024-04-15 05:22:14 +0200

    update products sql

---
+++ db-manager/src/main/resources/db/sql/product_details.sql
@@ -0,0 +1,11 @@
+insert into sellsphere_dev.product_details (id, name, detail_value, product_id)
+values  (95, 'Number of USB 2.0 Ports', '1', 273),
+        (96, ' Standing screen display size', '‎15.6 Inches', 273),
+        (97, 'Max Screen Resolution‎', '1920 x 1080 Pixels', 273),
+        (98, 'Screen Resolution‎', '1920 x 1080 pixels', 273),
+        (99, 'Number of USB 3.0 Ports', '2', 273),
+        (100, 'Hard Drive', '‎256 GB SSD', 273),
+        (101, 'Memory Speed', '‎2666 MHz', 273),
+        (102, 'Processor', '3.7 GHz ryzen_5_3550h', 273),
+        (103, 'Graphics Card Ram Size', '‎4 GB', 273),
+        (104, 'RAM', '‎8 GB DDR4', 273);
---
+++ db-manager/src/main/resources/db/sql/product_images.sql
@@ -0,0 +1,5 @@
+insert into sellsphere_dev.product_images (id, name, product_id)
+values  (9, '1_extra.jpg', 273),
+        (10, '2_extra.jpg', 273),
+        (11, '3_extra.jpg', 273),
+        (12, '4_extra.jpg', 273);
