c5725fa86ddb128382020c8ec4c04f783c78ad01
Marcin Seweryn <marcinsewerynn@gmail.com>
2024-06-11 01:41:32 +0200

    update product entity for easyship sync

--- common/src/main/java/com/sellsphere/common/entity/Product.java
+++ common/src/main/java/com/sellsphere/common/entity/Product.java
@@ -1,7 +1,6 @@
 package com.sellsphere.common.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -28,7 +27,7 @@ import java.util.Objects;
 @Entity
 @Table(name = "products")
 public class Product extends IdentifiedEntity {
-    
+
     /**
      * The name of the product.
      * This is a unique identifier that represents the product's name.
@@ -99,7 +98,8 @@ public class Product extends IdentifiedEntity {
      */
     @NotNull(message = "Cost is required")
     @DecimalMin(value = "0.0", inclusive = false, message = "Cost must be greater than zero")
-    @Digits(integer = 10, fraction = 2, message = "Cost must be a valid amount with up to 2 decimal places")
+    @Digits(integer = 10, fraction = 2, message = "Cost must be a valid amount with up to 2 " +
+            "decimal places")
     @Column(name = "cost", nullable = false, precision = 12, scale = 2)
     private BigDecimal cost;
 
@@ -109,7 +109,8 @@ public class Product extends IdentifiedEntity {
      */
     @NotNull(message = "Price is required")
     @DecimalMin(value = "0.0", inclusive = false, message = "Price must be greater than zero")
-    @Digits(integer = 10, fraction = 2, message = "Price must be a valid amount with up to 2 decimal places")
+    @Digits(integer = 10, fraction = 2, message = "Price must be a valid amount with up to 2 " +
+            "decimal places")
     @Column(name = "price", nullable = false, precision = 12, scale = 2)
     private BigDecimal price;
 
@@ -120,7 +121,8 @@ public class Product extends IdentifiedEntity {
     @NotNull(message = "Discount percent is required")
     @DecimalMin(value = "0.0", message = "Discount percent must be greater than or equal to zero")
     @DecimalMax(value = "100.00", message = "Discount percent must be less than or equal to 100.00")
-    @Digits(integer = 4, fraction = 2, message = "Discount percent must be a valid percentage with up to 2 decimal places")
+    @Digits(integer = 4, fraction = 2, message = "Discount percent must be a valid percentage " +
+            "with up to 2 decimal places")
     @Column(name = "discount_percent", nullable = false, precision = 4, scale = 2)
     private BigDecimal discountPercent = BigDecimal.ZERO;
 
@@ -130,7 +132,8 @@ public class Product extends IdentifiedEntity {
      */
     @NotNull(message = "Length is required")
     @DecimalMin(value = "0.0", inclusive = false, message = "Length must be greater than zero")
-    @Digits(integer = 10, fraction = 2, message = "Length must be a valid amount with up to 2 decimal places")
+    @Digits(integer = 10, fraction = 2, message = "Length must be a valid amount with up to 2 " +
+            "decimal places")
     @Column(name = "length", nullable = false, precision = 12, scale = 2)
     private BigDecimal length;
 
@@ -140,7 +143,8 @@ public class Product extends IdentifiedEntity {
      */
     @NotNull(message = "Width is required")
     @DecimalMin(value = "0.0", inclusive = false, message = "Width must be greater than zero")
-    @Digits(integer = 10, fraction = 2, message = "Width must be a valid amount with up to 2 decimal places")
+    @Digits(integer = 10, fraction = 2, message = "Width must be a valid amount with up to 2 " +
+            "decimal places")
     @Column(name = "width", nullable = false, precision = 12, scale = 2)
     private BigDecimal width;
 
@@ -150,7 +154,8 @@ public class Product extends IdentifiedEntity {
      */
     @NotNull(message = "Height is required")
     @DecimalMin(value = "0.0", inclusive = false, message = "Height must be greater than zero")
-    @Digits(integer = 10, fraction = 2, message = "Height must be a valid amount with up to 2 decimal places")
+    @Digits(integer = 10, fraction = 2, message = "Height must be a valid amount with up to 2 " +
+            "decimal places")
     @Column(name = "height", nullable = false, precision = 12, scale = 2)
     private BigDecimal height;
 
@@ -160,7 +165,8 @@ public class Product extends IdentifiedEntity {
      */
     @NotNull(message = "Weight is required")
     @DecimalMin(value = "0.0", inclusive = false, message = "Weight must be greater than zero")
-    @Digits(integer = 10, fraction = 2, message = "Weight must be a valid amount with up to 2 decimal places")
+    @Digits(integer = 10, fraction = 2, message = "Weight must be a valid amount with up to 2 " +
+            "decimal places")
     @Column(name = "weight", nullable = false, precision = 12, scale = 2)
     private BigDecimal weight;
 
@@ -202,7 +208,8 @@ public class Product extends IdentifiedEntity {
      * These details provide specific information about the product's features.
      */
     @OrderBy("name asc")
-    @OneToMany(mappedBy = "product", cascade = {CascadeType.ALL}, orphanRemoval = true, fetch = FetchType.EAGER)
+    @OneToMany(mappedBy = "product", cascade = {
+            CascadeType.ALL}, orphanRemoval = true, fetch = FetchType.EAGER)
     private List<ProductDetail> details = new ArrayList<>();
 
     /**
@@ -215,6 +222,39 @@ public class Product extends IdentifiedEntity {
     @ManyToOne
     @JoinColumn(name = "tax_id")
     private ProductTax tax;
+
+    // easyship integration
+    /**
+     * This boolean field indicates whether the item contains lithium-ion batteries that are
+     * packed with equipment. These are rechargeable batteries commonly used in consumer
+     * electronics, such as smartphones, laptops, and other portable devices.
+     * Packing Instruction 966 must be applied : Applies when lithium-ion batteries are packed
+     * separately from the equipment but shipped together in the same package
+     */
+    @Column(name = "contains_baterry_pi966", nullable = false)
+    private boolean containsBatteryPi966;
+
+    /**
+     * Packing Instruction 967: Applies when lithium-ion batteries are installed in the equipment
+     * being shipped.
+     * ContainsBatteryPi967: This boolean field indicates whether the item contains lithium-ion
+     * batteries that are installed in equipment.
+     * Lithium-Ion Batteries: As above, these are rechargeable batteries used in various
+     * electronic devices.
+     */
+    @Column(name = "contains_baterry_pi967", nullable = false)
+    private boolean containsBatteryPi967;
+
+    @Column(name = "contains_liquids", nullable = false)
+    private boolean containsLiquids;
+
+    /**
+     * Harmonized Commodity Description and Coding System. An HS code consists of at least six
+     * digits and is used by customs to classify the product being shipped. That way it can
+     * accurately calculate taxes and duties and apply any necessary restrictions.
+     */
+    @Column(name = "hs_code", nullable = false)
+    private String hsCode;
 
     public Product(Integer id) {
         this.id = id;
@@ -235,7 +275,8 @@ public class Product extends IdentifiedEntity {
     @Transient
     public BigDecimal getDiscountPrice() {
         if (discountPercent != null && discountPercent.compareTo(BigDecimal.ZERO) > 0) {
-            BigDecimal discountMultiplier = BigDecimal.valueOf(100).subtract(discountPercent).divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP);
+            BigDecimal discountMultiplier = BigDecimal.valueOf(100).subtract(
+                    discountPercent).divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP);
             return price.multiply(discountMultiplier);
         }
 
@@ -282,8 +323,12 @@ public class Product extends IdentifiedEntity {
     public final boolean equals(Object o) {
         if (this == o) return true;
         if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ?
+                ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() :
+                o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ?
+                ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() :
+                this.getClass();
         if (thisEffectiveClass != oEffectiveClass) return false;
         Product product = (Product) o;
         return getId() != null && Objects.equals(getId(), product.getId());
@@ -291,6 +336,7 @@ public class Product extends IdentifiedEntity {
 
     @Override
     public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+        return this instanceof HibernateProxy ?
+                ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
