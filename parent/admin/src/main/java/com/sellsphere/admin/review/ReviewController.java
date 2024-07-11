207105e38d1d927226905f7928f94adc9f5394f1
Marcin Seweryn <marcinsewerynn@gmail.com>
2024-07-12 01:48:07 +0200

    add admin review form

--- parent/admin/src/main/java/com/sellsphere/admin/review/ReviewController.java
+++ parent/admin/src/main/java/com/sellsphere/admin/review/ReviewController.java
@@ -2,8 +2,11 @@ package com.sellsphere.admin.review;
 
 import com.sellsphere.admin.page.PagingAndSortingHelper;
 import com.sellsphere.admin.page.PagingAndSortingParam;
+import com.sellsphere.common.entity.Review;
+import com.sellsphere.common.entity.ReviewNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -26,6 +29,15 @@ public class ReviewController {
         reviewService.listPage(pageNum, helper);
 
         return "review/reviews";
+    }
+
+    @GetMapping("/reviews/edit/{id}")
+    public String showReviewForm(@PathVariable("id") Integer id,
+                                 Model model) throws ReviewNotFoundException {
+        Review review = reviewService.get(id);
+        model.addAttribute("review", review);
+
+        return "review/review_form";
     }
 
 }
