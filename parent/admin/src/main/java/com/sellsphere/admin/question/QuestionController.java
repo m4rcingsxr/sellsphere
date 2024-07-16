6617aed070691e9f3472b9770358717a49f62663
Marcin Seweryn <marcinsewerynn@gmail.com>
2024-07-16 08:17:10 +0200

    add admin update question functionality

--- parent/admin/src/main/java/com/sellsphere/admin/question/QuestionController.java
+++ parent/admin/src/main/java/com/sellsphere/admin/question/QuestionController.java
@@ -3,22 +3,23 @@ package com.sellsphere.admin.question;
 import com.sellsphere.admin.page.PagingAndSortingHelper;
 import com.sellsphere.admin.page.PagingAndSortingParam;
 import com.sellsphere.admin.user.UserService;
-import com.sellsphere.common.entity.Question;
-import com.sellsphere.common.entity.QuestionNotFoundException;
+import com.sellsphere.common.entity.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.security.Principal;
 
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
     private final QuestionService questionService;
+    private final UserService userService;
 
     public static final String DEFAULT_REDIRECT_URL =
             "redirect:/questions/page/0?sortField=askTime&sortDir=asc";
@@ -48,6 +49,18 @@ public class QuestionController {
             QuestionNotFoundException {
         prepareModalForQuestionForm(id, model);
         return "question/question_form";
+    }
+
+    @PostMapping("/questions/save")
+    public String saveQuestion(@ModelAttribute("question") Question question, Principal principal, RedirectAttributes ra) throws
+            UserNotFoundException {
+        String email = principal.getName();
+        User answerer = userService.get(email);
+
+        questionService.save(question, answerer);
+        ra.addFlashAttribute(Constants.SUCCESS_MESSAGE, "Successfully updated question id [" + question.getId() + "]");
+
+        return DEFAULT_REDIRECT_URL;
     }
 
     private void prepareModalForQuestionForm(Integer id, Model model) throws QuestionNotFoundException {
--- parent/admin/src/main/java/com/sellsphere/admin/question/QuestionService.java
+++ parent/admin/src/main/java/com/sellsphere/admin/question/QuestionService.java
@@ -3,9 +3,12 @@ package com.sellsphere.admin.question;
 import com.sellsphere.admin.page.PagingAndSortingHelper;
 import com.sellsphere.common.entity.Question;
 import com.sellsphere.common.entity.QuestionNotFoundException;
+import com.sellsphere.common.entity.User;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
 
 @Transactional
 @RequiredArgsConstructor
@@ -25,4 +28,10 @@ public class QuestionService {
     }
 
 
+    public Question save(Question question, User answerer) {
+        question.setAnswerTime(LocalDate.now());
+        question.setAnswerer(answerer.getFullName());
+
+        return questionRepository.save(question);
+    }
 }
